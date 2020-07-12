9683b569dbecf0de773aa73d37cc8eb0fd8eb730
elvingonzalezb <ing.elvingonzalez@gmail.com>
2020-07-12 03:48:45 -0500

    update

--- src/app/componentes/Navbar.js
+++ src/app/componentes/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <Link className="navbar-brand" to="/">API Plaza Vea</Link>
+        <Link className="navbar-brand" to="/">API Plaza-Vea</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
